555b6b7444263eda692df69386c9121e787f6472
fadel ahmed <fadelsegaf@gmail.com>
2024-06-06 13:46:59 +0700

    Add init and update function to make it easy loop for the game

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,29 @@
 #include "Main.h"
 #include <iostream>
+#include <thread>
 
 
-int main()
+void init()
 {
 
-	std::cout << "fourth commit" << "\n";
-	return 0;
+}
+
+void update()
+{
+
+}
+
+int main() {
+    //Main loop
+    init();
+    
+    while (true) {
+        update();
+        // Optional: Sleep for a short duration to prevent maxing out the CPU
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+
+
+    return 0;
 }
